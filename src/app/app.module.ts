4af3232f8026e9abf07c55c2cc711e9284e9b1e8
Harles-Herman Pilter <harles.pilter@gmail.com>
2018-02-05 21:17:50 +0200

    Boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { storeFreeze } from 'ngrx-store-freeze';
 import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { environment } from '../environments/environment';
 
@@ -13,18 +13,31 @@ import { AppComponent } from './app.component';
 import { effects } from './store/effects';
 import { State, reducers } from './store/reducers';
 import { CustomSerializer } from './store/reducers/router.reducer';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesModule } from './movies/movies.module';
+import { SharedModule } from './shared/shared.module';
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [storeFreeze] : [];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: MoviesComponent,
+    data: { title: 'Popular movies' },
+  },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([]),
+    SharedModule,
+    RouterModule.forRoot(appRoutes),
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot(effects),
     StoreRouterConnectingModule,
     !environment.production ? StoreDevtoolsModule.instrument() : [],
+    MoviesModule,
   ],
   providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
   bootstrap: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [],
+  declarations: [NavigationComponent],
   providers: [],
-  exports: [CommonModule, FormsModule],
+  exports: [CommonModule, FormsModule, NavigationComponent],
 })
 export class SharedModule {}
