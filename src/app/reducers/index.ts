2b801a4801471353ac8397b355eb8d5be653d02c
Harles-Herman Pilter <harles.pilter@gmail.com>
2018-02-05 13:31:30 +0200

    Boilerplate

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,8 @@
+import { ActionReducer, ActionReducerMap, createFeatureSelector, createSelector, MetaReducer } from '@ngrx/store';
+import { environment } from '../../environments/environment';
+
+export interface IState {}
+
+export const reducers: ActionReducerMap<IState> = {};
+
+export const metaReducers: MetaReducer<IState>[] = !environment.production ? [] : [];
