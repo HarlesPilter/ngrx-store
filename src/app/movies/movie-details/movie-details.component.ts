c9b6ee8a2275461eed6af8d5fdb30a52ea1b4a72
Harles-Herman Pilter <harles.pilter@gmail.com>
2018-02-07 00:48:45 +0200

    Movie details

---
+++ src/app/movies/movie-details/movie-details.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Movie } from '../movies.model';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MovieDetailsComponent implements OnInit {
+  @Input() movie: Movie;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
